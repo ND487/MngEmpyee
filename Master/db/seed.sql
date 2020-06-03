4403795fb405ac003b38cd68a601746850aa98d0
GitNAG0 <fladnatas@gmail.com>
2020-06-02 18:03:33 -0700

    Added Homework

--- Master/db/seed.sql
+++ Master/db/seed.sql
@@ -24,10 +24,10 @@ INSERT INTO employee
     (first_name, last_name, role_id, manager_id)
 VALUES
     ('John', 'Doe', 1, NULL),
-    ('Mike', 'Chan', 2, 1),
-    ('Ashley', 'Rodriguez', 3, NULL),
-    ('Kevin', 'Tupik', 4, 3),
-    ('Kunal', 'Singh', 5, NULL),
-    ('Malia', 'Brown', 6, 5),
-    ('Sarah', 'Lourd', 7, NULL),
-    ('Tom', 'Allen', 8, 7);
+    ('Nate', 'Dogg', 2, 1),
+    ('Andrew', 'Reynolds', 3, NULL),
+    ('Stan', 'Marsh', 4, 3),
+    ('Hatsune', 'Miku', 5, NULL),
+    ('Luigi', 'Mario', 6, 5),
+    ('Suzy', 'Barlow', 7, NULL),
+    ('Andy', 'Mac', 8, 7);
